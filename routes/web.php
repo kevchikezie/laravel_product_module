b2efc3ca931094464a9a9e7eb1021a0e18408a0c
Kelvin Chikezie <kevchike@gmail.com>
2025-08-23 11:54:05 +0100

    Fetch products from database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
